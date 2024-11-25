cc6c029be3d7586cdd3bc1375e09bdc5de97c44f
Yunqian Fan <pannenets.f@foxmail.com>
2024-11-25 15:38:04 +0800

    Update mogan-research.rb

--- Casks/mogan-research.rb
+++ Casks/mogan-research.rb
@@ -1,7 +1,7 @@
 cask "mogan-research" do
   arch arm: "-arm"
 
-  version "1.2.9.3"
+  version "1.2.9.7-rc4"
   sha256 arm:   "74cb464ddde67d8c717c87362d5ad4b29c095be19de85ee9363f6b459ad05049",
          intel: "25bc5fc683bcc60ee720a5995b8ad5990fd702365baea5e7d0590973675cf746"
 
